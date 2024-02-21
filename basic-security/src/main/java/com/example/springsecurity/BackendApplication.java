e6f698764a56af18c03d09d49b9c1c112f38fac9
Rishi Singh <cs24rishi@gmail.com>
2024-02-21 15:42:29 +0530

    minor project naming updation

--- basic-security/src/main/java/com/example/springsecurity/BackendApplication.java
+++ basic-security/src/main/java/com/example/springsecurity/BackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
